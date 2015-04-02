daa803d5ee3f8785d69cad37cebbd254a86aa4f8
Sutkhet_T <SpeeDX_Fluke@hotmail.com>
2015-04-02 18:15:07 +0700

    initial hop4project

---
+++ hop4project/js/navbar.js
@@ -0,0 +1,14 @@
+<html> 
+  <head> 
+    <script src="jquery-1.11.2.min"></script> 
+    <script> 
+    $(function(){
+      $("#includedContent").load("./navbar.html"); 
+    });
+    </script> 
+  </head> 
+
+  <body> 
+     <div id="includedContent"></div>
+  </body> 
+</html>
